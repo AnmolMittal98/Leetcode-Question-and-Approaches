dadf073b1703394f6e4e8bf50782a6a975906959
Anmol Mittal <98anmolm@gmail.com>
2022-01-17 14:44:33 -0800

    Time: 48 ms (19.74%), Space: 57.2 MB (12.58%) - LeetHub

---
+++ 828-count-unique-characters-of-all-substrings-of-a-given-string/828-count-unique-characters-of-all-substrings-of-a-given-string.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int uniqueLetterString(String s) {
+          int[][] index = new int[26][2]; // {prevOfPrevI, prevI}
+          for(int i=0;i<26;i++){
+              Arrays.fill(index[i],-1);
+          }
+        
+        int res=0;
+        int mod=(int)Math.pow(10,9)+7;
+        for(int i=0;i<s.length();i++){
+            int c = s.charAt(i)-'A';
+            res=(res+(i-index[c][1]) * (index[c][1]-index[c][0])%mod)%mod;
+            index[c] = new int[]{index[c][1],i};
+        }
+        
+        for(int c=0;c<26;c++){
+          res=(res+(s.length()-index[c][1]) * (index[c][1] - index[c][0])%mod)%mod; 
+        }
+        return res;
+        
+    }
+}
